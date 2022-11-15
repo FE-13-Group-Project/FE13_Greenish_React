951eb7a00410625ddb7b28bdf01f2a29d7ff63a6
DaffaRadh <daffasven@gmail.com>
2022-11-15 13:51:09 +0700

    listEvent mk-1

--- FE13_GreenpeaceR/src/App.jsx
+++ FE13_GreenpeaceR/src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import ListEvent from './components/ListEvent'
 
 function App() {
 
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <ListEvent/>
     </div>
   )
 }
